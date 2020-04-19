819a71e7d43980999f104aeea09e974be0ea5457
vikash212000yadav <vikash212000yadav@gmail.com>
2020-04-19 21:40:16 +0530

    mini-max sum

---
+++ hackerrank/problem_solving/mini_max_sum.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the miniMaxSum function below.
+void miniMaxSum(vector<int> arr) {
+long int max= LONG_MIN;
+long int min= LONG_MAX;
+long int sum1=0;
+for(int i=0; i<5 ; i++)
+{   
+    for(int j=0; j<5; j++)
+    {
+        if(i!=j)
+        {
+            sum1 = sum1+arr.at(j);        
+        }
+    }
+    if(max<sum1)
+    {
+        max=sum1;
+    }
+    if(min>sum1)
+    {
+        min=sum1;
+    }
+    sum1=0;
+}
+cout<<min<<" "<<max;
+
+}
+
+int main()
+{
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(5);
+
+    for (int i = 0; i < 5; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    miniMaxSum(arr);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
