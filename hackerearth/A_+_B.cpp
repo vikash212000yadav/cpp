008434dfe8dbec2a46744d7547a4e840d9468ad0
vikash212000yadav <vikash212000yadav@gmail.com>
2020-04-25 14:50:08 +0530

    A+B

---
+++ hackerearth/A_+_B.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+#include <boost/multiprecision/cpp_int.hpp>
+
+
+
+using boost::multiprecision::cpp_int;
+
+using namespace std;
+
+
+
+int main(){
+
+cpp_int a, b;
+
+while(cin>>a>>b){
+
+cout<<a+b<<"\n";
+
+}
+
+return 0;
+
+}
