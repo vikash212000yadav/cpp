49334ed29150c786548f700b8cccc00547f3b6ec
vikash212000yadav <vikash212000yadav@gmail.com>
2020-05-20 10:00:30 +0530

    Day 14 -> Scopes

---
+++ hackerrank/30DaysOfCode/Day_14.cpp
@@ -0,0 +1,58 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+    Difference(vector<int> elements) 
+    {
+        this->elements = elements;
+    }
+
+    void computeDifference() 
+    {
+        int max = 0;
+
+        for (int i = 0; i < this->elements.size(); i++) {
+            for (int j = 0; j < this->elements.size(); j++) {
+                int a = abs(elements[i] - elements[j]);
+                if (a > max) 
+                    max = a;
+            }
+        }
+
+        maximumDifference = max;
+    }
+	// Add your code here
+
+}; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
