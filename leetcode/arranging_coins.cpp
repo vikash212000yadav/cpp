30e83dfa58dddd685a2e20219086226d561e2a45
vikash212000yadav <vikash212000yadav@gmail.com>
2020-07-01 14:44:05 +0530

    Arranging Coins

---
+++ leetcode/arranging_coins.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int count = 0;
+        int i = 1;
+        while(n) 
+        {
+            n = n - i;
+            if(n<0)
+                break;
+
+            count++;
+            i++;
+        }
+        return count;
+    }
+};
