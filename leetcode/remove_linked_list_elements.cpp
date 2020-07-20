9630e30c84197c5394a3bcf8dd18c3729b5d1a5d
vikash212000yadav <vikash212000yadav@gmail.com>
2020-07-20 14:24:03 +0530

    Remove Linked List Elements

---
+++ leetcode/remove_linked_list_elements.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* l = head;
+        
+        if(head == NULL)
+            return NULL;
+        
+        while(l->next != NULL)
+            if(l->next->val == val)
+                l->next = l->next->next;
+            else
+                l = l->next;
+            
+        if(head->val == val)
+            head = head->next;
+        
+        return head;
+    }
+};
